e0ff06314edda6641fe68545c94061299026c911
SengulC <sengulcagdal@gmail.com>
2024-02-26 14:16:19 +0000

    got the make opposite button working!
    inspired by: https://www.youtube.com/watch?v=fdiKq89iAeY

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
+import bodyParser from "body-parser";
 
 const key = process.env.VITE_OPENAI_KEY;
 const openai = new OpenAI({
   apiKey: key
 });
 
-let prompt = "yellow basketball"
+//https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
 async function callPrompt(prompt) {
   let content = "Come up with an object that's the opposite of: " + prompt;
   const completion = await openai.chat.completions.create({
@@ -18,17 +31,22 @@ async function callPrompt(prompt) {
   return completion.choices[0];
 }
 
-let result = await callPrompt(prompt);
-result = result.message.content;
-
 const app = express();
-
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/message", (req, res) => {
-  res.json({ message:  result});
+app.post("/gpt", async (req, res) => {
+  const {prompt} = req.body;
+  let result = await callPrompt(prompt);
+  // let result = makeid(5);
+  result = result.message.content;
+  res.send(result);
 });
+
+// app.get("/message", (req, res) => {
+//   res.json({ message:  result});
+// });
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
--- src/nodes/PostItNode.jsx
+++ src/nodes/PostItNode.jsx
@@ -2,37 +2,34 @@ import { useCallback, useRef, useState, useEffect} from 'react';
 import { Handle, Position } from 'reactflow';
 import '../index.css'
 import '../openai-test'
+import axios from 'axios';
 
 function PostIt({ data, isConnectable }) {
-  const onChange = useCallback((evt) => {
-    // console.log('changing node... ' + evt.target.value);
-  }, []);
-
   const inputRef = useRef(null);
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
 
-  const [message, setMessage] = useState("");
+  const artificial = (e) => {
+    e.preventDefault();
 
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
-  async function artificial() {
-    // let result = await callPrompt("football");
-    inputRef.current.innerHTML = message;
-  }
+    axios.post("http://localhost:8000/gpt", {prompt})
+    .then((res) => {
+      setResponse(res.data);
+      setPrompt(res.data);
+    })
+    .catch((err => {
+      console.error(err);
+    }))
+  };
 
   return (
     <div className='post-it-node'>
       <Handle type="target" position={Position.Top} isConnectable={isConnectable} />
-        <div ref={inputRef} className='nodrag post-it-text' name="text" contentEditable onChange={onChange} />
-        <button onClick={artificial}> Click </button>
+        <textarea ref={inputRef} className='nodrag post-it-text' name="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+        <button onClick={artificial}> Make-Opposite  </button>
       <Handle type="source" position={Position.Bottom} isConnectable={isConnectable} />
     </div>
   );
 }
-
-
 
 export default PostIt;
