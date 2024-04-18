52353be8c9a433fc2c4818e6a62b1fb6d23c8a1c
SengulC <sengulcagdal@gmail.com>
2024-04-18 16:54:33 +0100

    / typo??

--- server/index.js
+++ server/index.js
@@ -111,7 +111,7 @@ async function callButtonPrompt(prompt, input, brief, nodes) {
 const app = express();
 app.use(bodyParser.json());
 // app.use(cors());
-const allowedOrigins = 'https://guai-client.vercel.app/'
+const allowedOrigins = 'https://guai-client.vercel.app'
 const corsOptions = {
   origin: allowedOrigins,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
