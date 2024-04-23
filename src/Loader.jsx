f7506832c26bd00978f1bed6eb123b08b54c1903
SengulC <sengulcagdal@gmail.com>
2024-04-23 15:51:06 +0100

    loader image

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -4,9 +4,12 @@ export function Loader({ loadingState }) {
 
   return (
     <aside hidden={loadingState} className="loader">
-        <div>LOADER</div>
+        <img src="images/loader.png" alt="AI preloader"></img>
     </aside>
   );
 }
+
+// CREDITING PNG <a href="https://www.flaticon.com/free-icons/artificial-intelligence" title="artificial intelligence icons">Artificial intelligence icons created by Freepik - Flaticon</a>
+// MENU PNG <a href="https://www.flaticon.com/free-icons/robot" title="robot icons">Robot icons created by juicy_fish - Flaticon</a>
 
 export default Loader;
